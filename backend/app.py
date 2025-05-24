10a92f5fc6589cf52b50201d7135817182161242
Suprith R.Y <109782151+Suprith37@users.noreply.github.com>
2025-05-24 21:22:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,22 @@
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend communication
+CORS(app)
 
-# Connect to local MongoDB
-client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.1")
+# Use Mongo URI from environment variable
+mongo_uri = os.environ.get("MONGO_URI")
+client = MongoClient(mongo_uri)
 db = client["emptyCup"]
 collection = db["companies"]
 
-# GET all companies
 @app.route("/companies", methods=["GET"])
 def get_companies():
-    companies = list(collection.find({}, {"_id": 0}))  # exclude MongoDB _id
-    print(companies)
+    companies = list(collection.find({}, {"_id": 0}))
     return jsonify(companies)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+    app.run(host="0.0.0.0", port=5000)  # Required for Render
